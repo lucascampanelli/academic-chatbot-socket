a5976616cf3a20d83d25895158913b557cdd4881
Lucas Campanelli <lucas.campanelli@outlook.com>
2022-05-15 20:00:31 -0300

    Criando o banco de dados, populando e fazendo a conexão com o programa

---
+++ src/Server/DAO/DaoAluno.java
@@ -0,0 +1,9 @@
+package Server.DAO;
+
+/**
+ *
+ * @author Erick Pereira, Felipe Campos, Guilherme Rodrigues, Lucas Campanelli, Paulo Silveira e Ronaldo Arley
+ */
+public class DaoAluno {
+    
+}
---
+++ src/Server/DAO/Factory/Connect.java
@@ -0,0 +1,72 @@
+package Server.DAO.Factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Erick Pereira, Felipe Campos, Guilherme Rodrigues, Lucas Campanelli, Paulo Silveira e Ronaldo Arley
+ */
+public class Connect {
+    
+    // Url do banco de dados que será conectado
+    private static final String url = "jdbc:sqlite:university.db";
+    // Variável que armazenará a conexão com o banco
+    private static Connection conn = null;
+    
+    // Método responsável por conectar o banco de dados
+    public static void connect(){
+        try{
+            Connect.conn = DriverManager.getConnection(url);
+            System.out.println("Banco de dados conectado com sucesso!");
+        } catch(SQLException e){
+            System.out.println("Ocorreu um erro ao conectar com o banco de dados.\nDetalhes do erro: " + e.getMessage());
+        }
+    }
+    
+    // Método responsável por desconectar o banco de dados
+    public static void desconnect(){
+        try{
+            if (Connect.conn != null) {
+                Connect.conn.close();
+                System.out.println("Banco de dados desconectado.");
+            }
+            else{
+                System.out.println("Sem conexão com banco de dados criada.");
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("Ocorreu um erro ao desconectar o banco de dados.\nDetalhes do erro: " + e.getMessage());
+        }
+    }
+    
+    public static Statement getStatement(){
+        try{
+            Statement statement = Connect.conn.createStatement();
+            return statement;
+        }
+        catch(SQLException e){
+            System.out.println("Ocorreu um erro ao criar o statement para o banco de dados.\nDetalhes do erro: " + e.getMessage());
+        }
+        return null;
+    }
+    
+    public static PreparedStatement getPreparedStatement(String sqlQuery){
+        try{
+            PreparedStatement preparedStatement = Connect.conn.prepareStatement(sqlQuery);
+            return preparedStatement;
+        }
+        catch(SQLException e){
+            System.out.println("Ocorreu um erro ao criar o prepared statement para o banco de dados.\nDetalhes do erro: " + e.getMessage());
+            return null;
+        }
+    }
+    
+    // Método responsável por informar se a conexão com o banco de dados existe
+    public static boolean exists(){
+        return Connect.conn != null;
+    }
+}
